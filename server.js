586bbb10a90626277e707b4d532333b6cb57fc20
thesides <aaronm20392@gmail.com>
2017-10-31 20:27:22 -0500

    dynamic port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser")
 
 var app = express();
-var PORT = 8080;
+var PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());

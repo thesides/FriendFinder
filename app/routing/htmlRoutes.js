ec40ab75ec14ccfb677a32814b60cdb551c048bb
thesides <aaronm20392@gmail.com>
2017-11-01 17:40:34 -0500

    fixed routing again

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -16,4 +16,8 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../public/home.html"))
   });
 
+  app.get("/", function (req, res){
+    res.sendFile(path.join(__dirname, "../public/home.html"))
+  });
+
 };
